4ab2842c4ce6d469097a159bab25597128eb6be8
Pratham <prathamgarg1502@gmail.com>
2026-06-22 07:55:03 +0530

    feat: Implement checkSumUtility to Secure and avoid redundancy of Data in uploaded files

---
+++ src/main/java/enterpriseapplication/enterpriseapplication_minio/util/FileStatus.java
@@ -0,0 +1,13 @@
+package enterpriseapplication.enterpriseapplication_minio.util;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public final class FileStatus {
+    public static final String ACTIVE = "ACTIVE";
+
+    public static final String DELETED = "DELETED";
+
+    public static final String ARCHIVED = "ARCHIVED";
+
+}
